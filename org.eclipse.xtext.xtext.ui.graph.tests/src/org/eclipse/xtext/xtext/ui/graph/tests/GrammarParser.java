ef72cf7bf825adf20a993cd36bde067a90ea6bc3
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2025-03-05 06:50:04 +0100

    just a test

--- org.eclipse.xtext.xtext.ui.graph.tests/src/org/eclipse/xtext/xtext/ui/graph/tests/GrammarParser.java
+++ org.eclipse.xtext.xtext.ui.graph.tests/src/org/eclipse/xtext/xtext/ui/graph/tests/GrammarParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2022 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2010, 2024 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -8,24 +8,23 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.ui.graph.tests;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.util.StringInputStream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 public class GrammarParser {
 
 	public Grammar parse(String rules) throws IOException {
-		ResourceSet resourceSet = new XtextResourceSet();
+		XtextResourceSet resourceSet = new XtextResourceSet();
+		resourceSet.setClasspathURIContext(GrammarParser.class);
 		Resource grammarResource = resourceSet.createResource(URI.createURI("Test.xtext"));
 		grammarResource.load(new StringInputStream(
 				"grammar Test with org.eclipse.xtext.common.Terminals \n"
